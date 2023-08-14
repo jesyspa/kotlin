959be1903c9924517cbfd855a028a0fdded3f05e
Nils Cremer <nils.crmr@gmail.com>
2023-08-14 15:18:45 +0200

    Add nullable conversion for smart casts

--- plugins/formal-verification/testData/diagnostics/no_contracts/nullable.kt
+++ plugins/formal-verification/testData/diagnostics/no_contracts/nullable.kt
@@ -1,8 +1,8 @@
-fun return_nullable(): Int? {
+fun <!VIPER_TEXT!>return_nullable<!>(): Int? {
     return 0
 }
 
-fun smart_cast(x: Int?): Int {
+fun <!VIPER_TEXT!>smart_cast<!>(x: Int?): Int {
     if (x == null) {
         return 0
     } else {
@@ -10,13 +10,13 @@ fun smart_cast(x: Int?): Int {
     }
 }
 
-fun use_nullable_twice(x: Int?): Int? {
+fun <!VIPER_TEXT!>use_nullable_twice<!>(x: Int?): Int? {
     val a = x
     val b = x
     return a
 }
 
-fun pass_nullable_parameter(x: Int?): Int? {
+fun <!VIPER_TEXT!>pass_nullable_parameter<!>(x: Int?): Int? {
     use_nullable_twice(x)
     return x
 }
