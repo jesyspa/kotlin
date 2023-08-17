c987ba3eb5e7998cd0dea2c2702f4e09100e95cb
francescoo22 <francesco.protopapa22@gmail.com>
2023-08-17 11:33:46 +0200

    support for every type in contracts null checks

--- plugins/formal-verification/src/org/jetbrains/kotlin/formver/conversion/ContractDescriptionConversionVisitor.kt
+++ plugins/formal-verification/src/org/jetbrains/kotlin/formver/conversion/ContractDescriptionConversionVisitor.kt
@@ -11,13 +11,24 @@ import org.jetbrains.kotlin.fir.diagnostics.ConeDiagnostic
 import org.jetbrains.kotlin.fir.types.ConeKotlinType
 import org.jetbrains.kotlin.formver.scala.silicon.ast.Exp
 import org.jetbrains.kotlin.formver.scala.silicon.ast.Exp.*
-import org.jetbrains.kotlin.formver.scala.silicon.ast.Type
 
 class ContractDescriptionConversionVisitor : KtContractDescriptionVisitor<Exp, MethodConversionContext, ConeKotlinType, ConeDiagnostic>() {
     private fun KtValueParameterReference<ConeKotlinType, ConeDiagnostic>.convertedVar(data: MethodConversionContext): ConvertedVar {
         val name = data.signature.params[parameterIndex].name
         val type = data.signature.params[parameterIndex].type
         return ConvertedVar(name, type)
+    }
+
+    private fun ConvertedVar.nullCmp(isNegated: Boolean): Exp {
+        return if (type is ConvertedNullable) {
+            if (isNegated) {
+                NeCmp(toLocalVar(), NullableDomain.nullVal(type.elementType.viperType))
+            } else {
+                EqCmp(toLocalVar(), NullableDomain.nullVal(type.elementType.viperType))
+            }
+        } else {
+            BoolLit(isNegated)
+        }
     }
 
     override fun visitBooleanConstantDescriptor(
@@ -38,9 +49,13 @@ class ContractDescriptionConversionVisitor : KtContractDescriptionVisitor<Exp, M
         val retVar = data.returnVar.toLocalVar()
         return when (returnsEffect.value) {
             ConeContractConstantValues.WILDCARD -> BoolLit(true)
-            // TODO: extend null comparisons to other types (also non-nullable), I think it is better waiting for null to work with more types
-            ConeContractConstantValues.NULL -> EqCmp(retVar, NullableDomain.nullVal(Type.Int))
-            ConeContractConstantValues.NOT_NULL -> NeCmp(retVar, NullableDomain.nullVal(Type.Int))
+            /* NOTE: in a function that has a non-nullable return type, the compiler will not complain if there is an effect like
+             * returnsNotNull(). So it is necessary to take care of these cases in order to avoid comparison between non-nullable
+             * values and null. In a function that has a non-nullable return type, returnsNotNull() is mapped to true and returns(null)
+             * is mapped to false
+             */
+            ConeContractConstantValues.NULL -> data.returnVar.nullCmp(false)
+            ConeContractConstantValues.NOT_NULL -> data.returnVar.nullCmp(true)
             ConeContractConstantValues.TRUE -> EqCmp(retVar, BoolLit(true))
             ConeContractConstantValues.FALSE -> EqCmp(retVar, BoolLit(false))
             else -> throw Exception("Unexpected constant: ${returnsEffect.value}")
@@ -56,13 +71,12 @@ class ContractDescriptionConversionVisitor : KtContractDescriptionVisitor<Exp, M
         isNullPredicate: KtIsNullPredicate<ConeKotlinType, ConeDiagnostic>,
         data: MethodConversionContext
     ): Exp {
-        val arg = isNullPredicate.arg.accept(this, data)
-        // TODO: extend null comparisons to other types (also non-nullable), I think it is better waiting for null to work with more types
-        return if (isNullPredicate.isNegated) {
-            NeCmp(arg, NullableDomain.nullVal(Type.Int))
-        } else {
-            EqCmp(arg, NullableDomain.nullVal(Type.Int))
-        }
+        /* NOTE: useless comparisons like x != null with x non-nullable will compile with just a warning.
+         * So it is necessary to take care of these cases in order to avoid comparison between non-nullable
+         * values and null. Let x be a non-nullable variable, then x == null is mapped to false and x != null is mapped to true
+         */
+        val arg = data.signature.params[isNullPredicate.arg.parameterIndex]
+        return arg.nullCmp(isNullPredicate.isNegated)
     }
 
     override fun visitLogicalBinaryOperationContractExpression(
--- plugins/formal-verification/testData/diagnostics/returns_null.kt
+++ plugins/formal-verification/testData/diagnostics/returns_null.kt
@@ -11,7 +11,7 @@ fun <!VIPER_TEXT!>simple_returns_null<!>(x: Int?): Int? {
 }
 
 @OptIn(ExperimentalContracts::class)
-fun <!VIPER_TEXT!>returns_null_implies<!>(x: Int?): Int? {
+fun <!VIPER_TEXT!>returns_null_implies<!>(x: Boolean?): Boolean? {
     contract {
         returns(null) implies (x == null)
         returnsNotNull() implies (x != null)
@@ -30,4 +30,28 @@ fun <!VIPER_TEXT!>returns_null_with_if<!>(x: Int?, y: Int?, z: Int?): Int? {
     } else {
         return z
     }
+}
+
+@OptIn(ExperimentalContracts::class)
+fun <!VIPER_TEXT!>non_nullable_returns_not_null<!>(x: Int): Int {
+    contract {
+        returnsNotNull()
+    }
+    return x
+}
+
+@OptIn(ExperimentalContracts::class)
+fun <!FUNCTION_WITH_UNVERIFIED_CONTRACT, VIPER_ERROR, VIPER_TEXT!>non_nullable_returns_null<!>(x: Int): Int {
+    contract {
+        returns(null)
+    }
+    return x
+}
+
+@OptIn(ExperimentalContracts::class)
+fun <!VIPER_TEXT!>non_nullable_compared_to_null<!>(x: Int, y: Int): Int {
+    contract {
+        returns() implies (<!SENSELESS_COMPARISON!>y == null<!> || <!SENSELESS_COMPARISON!>x != null<!>)
+    }
+    return x
 }
