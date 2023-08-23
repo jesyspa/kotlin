e73203c0b16fbeda8a7caf8d2e6365fe884edacf
Nils Cremer <nils.crmr@gmail.com>
2023-08-23 17:51:49 +0200

    Simplify tests

--- plugins/formal-verification/testData/diagnostics/no_contracts/loop.kt
+++ plugins/formal-verification/testData/diagnostics/no_contracts/loop.kt
@@ -1,17 +1,11 @@
-fun <!VIPER_TEXT!>while_loop<!>(b: Boolean): Int {
+fun <!VIPER_TEXT!>while_loop<!>(b: Boolean): Boolean {
     while (b) {
         val a = 1
         val c = 2
     }
-    return 0
-}
-
-fun <!VIPER_TEXT!>returns_bool<!>(): Boolean {
     return false
 }
 
 fun <!VIPER_TEXT!>while_function_condition<!>() {
-    while (returns_bool()) {
-
-    }
+    while (while_loop(true)) { }
 }
