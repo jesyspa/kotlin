2b982db0d06a88df5f45562cf8a04d957a0829ec
francescoo22 <francesco.protopapa22@gmail.com>
2023-09-08 09:45:25 +0200

    Fix tests

--- plugins/formal-verification/testData/diagnostics/no_contracts/loop.kt
+++ plugins/formal-verification/testData/diagnostics/no_contracts/loop.kt
@@ -9,34 +9,3 @@ fun <!VIPER_TEXT!>while_loop<!>(b: Boolean): Boolean {
 fun <!VIPER_TEXT!>while_function_condition<!>() {
     while (while_loop(true)) { }
 }
-
-fun <!VIPER_TEXT!>while_break<!>(b: Boolean): Int {
-    var i = 0
-    while (b) {
-        i = 1
-        break
-    }
-    return i
-}
-
-fun <!VIPER_TEXT!>while_continue<!>(b: Boolean): Int {
-    var i = 0
-    while (b) {
-        i = 1
-        continue
-    }
-    return i
-}
-
-fun <!VIPER_TEXT!>while_nested<!>(b: Boolean) {
-    while(b){
-        while (b){
-            break
-        }
-        continue
-        while (b){
-            continue
-        }
-        break
-    }
-}
--- plugins/formal-verification/testData/diagnostics/no_contracts/return_break_continue.kt
+++ plugins/formal-verification/testData/diagnostics/no_contracts/return_break_continue.kt
@@ -9,3 +9,34 @@ fun <!VIPER_TEXT!>return_from_loop<!>(): Int {
     }
     return 1
 }
+
+fun <!VIPER_TEXT!>while_break<!>(b: Boolean): Int {
+    var i = 0
+    while (b) {
+        i = 1
+        break
+    }
+    return i
+}
+
+fun <!VIPER_TEXT!>while_continue<!>(b: Boolean): Int {
+    var i = 0
+    while (b) {
+        i = 1
+        continue
+    }
+    return i
+}
+
+fun <!VIPER_TEXT!>while_nested<!>(b: Boolean) {
+    while(b){
+        while (b){
+            break
+        }
+        continue
+        while (b){
+            continue
+        }
+        break
+    }
+}
