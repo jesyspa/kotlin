04314338d720456cf4b7c2849da5ff60fceb15b5
Nils Cremer <nils.crmr@gmail.com>
2023-09-25 11:48:06 +0200

    Support elvis operator (`?:`)

--- plugins/formal-verification/testData/diagnostics/no_contracts/nullable.kt
+++ plugins/formal-verification/testData/diagnostics/no_contracts/nullable.kt
@@ -20,3 +20,11 @@ fun <!VIPER_TEXT!>nullable_non_nullable_comparison<!>(x: Int?, y: Int?): Boolean
 fun <!VIPER_TEXT!>null_comparison<!>(x: Int?): Boolean {
     return x == null
 }
+
+fun <!VIPER_TEXT!>elvis_operator<!>(x: Int?): Int {
+    return x ?: 3
+}
+
+fun <!VIPER_TEXT!>elvis_operator_complex<!>(x: Int?): Int {
+    return pass_nullable_parameter(x) ?: elvis_operator(2)
+}
