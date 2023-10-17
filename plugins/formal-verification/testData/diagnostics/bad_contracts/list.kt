d8c1da2859fa5f778f74f415f616176811d633e3
Gabriele Pappalardo <gabriele.pappalardo@jetbrains.com>
2023-10-17 11:29:36 +0200

    Fix annotation error on list test

--- plugins/formal-verification/testData/diagnostics/bad_contracts/list.kt
+++ plugins/formal-verification/testData/diagnostics/bad_contracts/list.kt
@@ -5,16 +5,16 @@ import org.jetbrains.kotlin.formver.plugin.AlwaysVerify
 @AlwaysVerify
 fun <!FUNCTION_WITH_UNVERIFIED_CONTRACT, VIPER_TEXT!>empty_list_get<!>() {
     val myList: List<Int> = emptyList()
-    val s = myList[0]
+    val s = <!VIPER_VERIFICATION_ERROR!>myList[0]<!>
 }
 
 @AlwaysVerify
 fun <!FUNCTION_WITH_UNVERIFIED_CONTRACT, VIPER_TEXT!>unsafe_last<!>(l: List<Int>) : Int {
-    return l[l.size - 1]
+    return <!VIPER_VERIFICATION_ERROR!>l[l.size - 1]<!>
 }
 
 @AlwaysVerify
 fun <!FUNCTION_WITH_UNVERIFIED_CONTRACT, VIPER_TEXT!>add_get<!>(l: MutableList<Int>) {
     l.add(1)
-    val n = l[1]
+    val n = <!VIPER_VERIFICATION_ERROR!>l[1]<!>
 }
