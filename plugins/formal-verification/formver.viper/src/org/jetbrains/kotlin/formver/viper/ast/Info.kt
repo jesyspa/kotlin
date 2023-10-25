1251a3e9d3238a5173fd3195bbf3b72b512b0d28
Gabriele Pappalardo <gabriele.pappalardo@jetbrains.com>
2023-10-25 14:36:29 +0200

    Adding wrapped Info

--- plugins/formal-verification/formver.viper/src/org/jetbrains/kotlin/formver/viper/ast/Info.kt
+++ plugins/formal-verification/formver.viper/src/org/jetbrains/kotlin/formver/viper/ast/Info.kt
@@ -6,10 +6,38 @@
 package org.jetbrains.kotlin.formver.viper.ast
 
 import org.jetbrains.kotlin.formver.viper.IntoSilver
+import scala.collection.JavaConverters
+import scala.collection.immutable.Seq
 import viper.silver.ast.`NoInfo$`
 
 sealed class Info : IntoSilver<viper.silver.ast.Info> {
+
+    internal class Wrapper<I>(val wrappedValue: I) : viper.silver.ast.Info {
+        override fun toString(): String = "<wrapped info value>"
+        override fun comment(): Seq<String> = JavaConverters.asScala(emptyList<String>()).toSeq()
+        override fun isCached(): Boolean = false
+    }
+
+    companion object {
+        fun fromSilver(info: viper.silver.ast.Info): Info = when (info) {
+            `NoInfo$`.`MODULE$` -> NoInfo
+            is Wrapper<*> -> Wrapped(info.wrappedValue)
+            else -> TODO("Unreachable")
+        }
+    }
+
     data object NoInfo : Info() {
         override fun toSilver(): viper.silver.ast.Info = `NoInfo$`.`MODULE$`
     }
+
+    class Wrapped<I>(val info: I) : Info() {
+        override fun toSilver(): viper.silver.ast.Info = Wrapper(info)
+    }
 }
+
+inline fun <reified I> Info.unwrapOr(orBlock: () -> I?): I? = when (this) {
+    is Info.Wrapped<*> -> info as? I
+    else -> orBlock()
+}
+
+fun Info.isEmpty(): Boolean = (this is Info.NoInfo)
