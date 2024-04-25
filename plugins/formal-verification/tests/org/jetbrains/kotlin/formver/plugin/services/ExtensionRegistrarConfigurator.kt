0140cdd4680ade3b6fc3350416567e3b64da5a75
francescoo22 <francesco.protopapa22@gmail.com>
2024-04-25 10:40:57 +0200

    Prevent concurrent use of FULL_VIPER_DUMP and RENDER_PREDICATES

--- plugins/formal-verification/tests/org/jetbrains/kotlin/formver/plugin/services/ExtensionRegistrarConfigurator.kt
+++ plugins/formal-verification/tests/org/jetbrains/kotlin/formver/plugin/services/ExtensionRegistrarConfigurator.kt
@@ -24,6 +24,10 @@ class ExtensionRegistrarConfigurator(testServices: TestServices) : EnvironmentCo
         get() = listOf(FormVerDirectives)
 
     override fun ExtensionStorage.registerCompilerExtensions(module: TestModule, configuration: CompilerConfiguration) {
+        if (FULL_VIPER_DUMP in module.directives && RENDER_PREDICATES in module.directives) {
+            throw IllegalArgumentException("Directives FULL_VIPER_DUMP and RENDER_PREDICATES cannot be present in the same test file.")
+        }
+
         val logLevel = when {
             FULL_VIPER_DUMP in module.directives -> LogLevel.FULL_VIPER_DUMP
             RENDER_PREDICATES in module.directives -> LogLevel.SHORT_VIPER_DUMP_WITH_PREDICATES
