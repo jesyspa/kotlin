5b468009b3a2e38749b30c98a7cf1f5bd4defc6d
GrigoriiSolnyshkin <89751176+GrigoriiSolnyshkin@users.noreply.github.com>
2024-08-26 01:19:51 +0300

    Improving type guarantees when invoking (#233)
    With this pull request necessary casts are added when calling and
    returning from lambdas and functions (inline and non-inline).
    
    Before code like
    ```kotlin
    fun <T> id(x: T) = x
    
    inline fun <T, R> T.runWithId(block: T.() -> R) = id(this).block()
    
    class Class {
      val member = 0
    }
    
    Class().runWithId { member }
    ```
    wouldn't work: we only knew that the object returned from `id` had type
    `Any?`. Therefore, we make casts when entering (inline calls) and
    returning (from any calls) to keep `TypeEmbedding` consistent with
    kotlin types (this is also needed to correctly represent `==` calls
    etc.)

--- plugins/formal-verification/formver.core/src/org/jetbrains/kotlin/formver/embeddings/callables/CallableEmbedding.kt
+++ plugins/formal-verification/formver.core/src/org/jetbrains/kotlin/formver/embeddings/callables/CallableEmbedding.kt
@@ -7,7 +7,9 @@ package org.jetbrains.kotlin.formver.embeddings.callables
 
 import org.jetbrains.kotlin.formver.conversion.StmtConversionContext
 import org.jetbrains.kotlin.formver.embeddings.FunctionTypeEmbedding
+import org.jetbrains.kotlin.formver.embeddings.TypeEmbedding
 import org.jetbrains.kotlin.formver.embeddings.expression.ExpEmbedding
+import org.jetbrains.kotlin.formver.embeddings.expression.withNewTypeInvariants
 
 /**
  * Kotlin entity that can be called.
@@ -16,3 +18,9 @@ interface CallableEmbedding {
     val type: FunctionTypeEmbedding
     fun insertCall(args: List<ExpEmbedding>, ctx: StmtConversionContext): ExpEmbedding
 }
+
+fun CallableEmbedding.insertCall(args: List<ExpEmbedding>, ctx: StmtConversionContext, actualReturnType: TypeEmbedding) =
+    insertCall(args, ctx).withNewTypeInvariants(actualReturnType) {
+        access = true
+        proven = true
+    }
