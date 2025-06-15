a8488e6be12e4e3f61d7f0c9f05bd8a9d5e3263d
Sai Divvela <saidivvela2005@gmail.com>
2025-06-15 14:49:34 -0700

    got test working and generated viper code

--- plugins/formal-verification/testData/diagnostics/verifies/class.kt
+++ plugins/formal-verification/testData/diagnostics/verifies/class.kt
@@ -1,14 +1,16 @@
+// RENDER_PREDICATES
+
 import org.jetbrains.kotlin.formver.plugin.NeverConvert
 import org.jetbrains.kotlin.formver.plugin.AlwaysVerify
 import org.jetbrains.kotlin.formver.plugin.Unique
 import org.jetbrains.kotlin.formver.plugin.verify
 
 open class A(
-    open val a: Any,
-    open var b: Any,
+    val x: Int,
+    var y: Int,
 )
 
-open class B(
+class B(
     @property:Unique
     val a1: A,
     val a2: A,
@@ -16,6 +18,10 @@ open class B(
 
 class C(
     @property:Unique
-    override var b: B?,
+    var b: B?,
 ):A(0,0)
+
+fun <!VIPER_TEXT!>testElem<!>(@Unique instance: C) {
+    var abc = instance.b?.a1?.x
+}
 
